d59a596f8bf823ec6fd1869cb35d4ea1cd209566
Fadhil Kurnia <fadhilimamk@gmail.com>
2023-04-02 13:59:52 -0400

    add readme, add bplustree

--- README.md
+++ README.md
@@ -25,3 +25,33 @@ cmake ..
 make
 ```
 The compilation produces a single binary file named `sedann`.
+
+
+______________
+
+# Handle Search Operation
+
+1. Get the dataset (SIFT10M)
+   First, download the dataset of 100 millions vectors.
+   ```
+    cd data
+    wget ftp://ftp.irisa.fr/local/texmex/corpus/bigann_learn.bvecs.gz
+   ```
+   Then run a script to get the first 10 millions vectors from the downloaded dataset for our measurement. The script also 
+   generates `c=10000` centroids and assigns each of the 10M vectors into one of the centroid. Thus, a cluster consists of a
+   centroid with multiple vectors. The script requires `faiss` installed via `conda`.
+   ```
+    python3 cluster_dataset.py
+   ```
+    In the end, the script produces `centroids_10k_sift10m.npy` and `clusters_10k_sift10m.npy` which contain the 10,000 
+    centroids and the cluster ID of each vector in the SIFT10M dataset.
+
+    > We potentially change this SIFT10M (extracted from bigann_learn.bvecs) with the first 10M vectors in `bigann_base.bvecs.gz` since it already provide the ground truth.
+
+2. Build the Graph Index for the Centroids
+   
+   
+3. Build the B+Tree Index while Calculating the Precomputed Distance (PCD)
+
+
+4. Receiving Search Requests
---
+++ script/cluster_dataset.py
@@ -0,0 +1,51 @@
+import faiss
+import numpy as np
+
+def bvecs_read(fname):
+    a = np.fromfile(fname, dtype=np.int32, count=1)
+    b = np.fromfile(fname, dtype=np.uint8)
+    d = a[0]
+    return b.reshape(-1, d + 4)[:, 4:].copy()
+
+def ivecs_read(fname):
+    a = np.fromfile(fname, dtype='int32')
+    d = a[0]
+    return a.reshape(-1, d + 1)[:, 1:].copy()
+
+def fvecs_read(fname):
+    return ivecs_read(fname).view('float32')
+
+
+# reading bvecs file
+filename = "./data/bigann_learn.bvecs"
+print(">>> start reading bvecs file (", filename, ") ...")
+dataset = bvecs_read(filename).astype(np.float32)
+print(">>> dataset is loaded")
+
+print(dataset[:10])
+
+print(">>> get the first 10M of vectors")
+D = 128
+N = 10000000
+C = 10000
+dataset = dataset[:N]
+
+kmeans = faiss.Kmeans(d=D, k=C, niter=50, nredo=3, verbose=True, seed=354)
+print(">>> running kmeans clustering ...")
+kmeans.train(dataset)
+print(">>> kmeans is done :)")
+print("     ", kmeans.obj)
+print("     ", kmeans.centroids.shape)
+
+print(">>> saving the centroids")
+np.save("./data/centroids_10k_sift10m.npy", kmeans.centroids)
+
+print(">>> assigning all vectors to a cluster")
+dists, ids = kmeans.index.search(dataset, 1)
+print("     ", ids.shape)
+print(">>> saving the clusters")
+np.save("./data/clusters_10k.npy", ids)
+
+print("D:", kmeans.d)
+print("C:", kmeans.k)
+print("niter:", kmeans.cp.niter)
