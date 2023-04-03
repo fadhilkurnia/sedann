d6623689f91b4c4a58c5fdfd43e3628399dec9b1
Fadhil Kurnia <fikurnia@umass.eduOA>
2023-04-02 20:43:25 -0400

    load query from the datset

--- src/test_faiss_graph.cpp
+++ src/test_faiss_graph.cpp
@@ -8,7 +8,7 @@
 // 64-bit int
 using idx_t = faiss::idx_t;
 
-float* read_fvecs(char *filename) {
+float* read_fvecs(const char *filename) {
     std::ifstream reader(filename, std::ios::binary | std::ios::ate);
     size_t fsize = reader.tellg();
     reader.seekg(0, std::ios::beg);
@@ -38,28 +38,50 @@ float* read_fvecs(char *filename) {
     return data;
 }
 
+float* read_bvecs(const char *filename) {
+    std::ifstream reader(filename, std::ios::binary | std::ios::ate);
+    size_t fsize = reader.tellg();
+    reader.seekg(0, std::ios::beg);
+
+    uint32_t ndims_u32;
+    reader.read((char *)&ndims_u32, sizeof(uint32_t));
+    reader.seekg(0, std::ios::beg);
+    size_t ndims = (size_t) ndims_u32;
+    size_t nvecs = fsize / ((ndims + 4) * sizeof(uint8_t));
+    std::cout << ">> Dataset (" << filename << "): #vector = " << nvecs << ", #dims = " << ndims << std::endl;
+
+    char* buffer = new char[nvecs * ((4 + ndims) * sizeof(uint8_t))];
+    reader.read(buffer, nvecs * ((4 + ndims) * sizeof(uint8_t)));
+    
+    float* data = new float[nvecs * ndims * sizeof(float)];
+
+    for (size_t vid=0; vid < nvecs; vid++) {
+        for (size_t d=0; d < ndims; d++) {
+            data[vid * ndims + d] = (float) *(buffer + vid * (4 + ndims) + d);
+        }
+    }
+    
+    reader.close();
+    delete[] buffer;
+    
+    return data;
+}
+
 int main() {
     int d = 128;    // dimension
     int nb = 10000; // database size (number of centroids)
-    int nq = 10;    // number of search queries
+    int nq = 10000;    // number of search queries
 
     // read the centroids
-    char* filename = "../data/centroids_10k_sift10m.fvecs";
-    float* centroids = read_fvecs(filename);
+    const char* centroid_filename = "../data/centroids_10k_sift10m.fvecs";
+    float* centroids = read_fvecs(centroid_filename);
 
-    std::mt19937 rng;
-    std::uniform_real_distribution<> distrib(0.0, 255.0);
+    // read the query
+    const char* query_filename = "../data/bigann_query.bvecs";
+    float* queries = read_bvecs(query_filename);
 
     float* xb = centroids;
-    float* xq = new float[d * nq];
-
-    { 
-        printf(">> initialize queries ...\n");
-        for (int i = 0; i < nq; i++) {
-            for (int j = 0; j < d; j++)
-                xq[d * i + j] = distrib(rng);
-        }
-    }
+    float* xq = queries;
 
     printf(">> creating the index\n");
     faiss::IndexNSGFlat index(d, 32);
@@ -78,18 +100,21 @@ int main() {
         // print out results
         printf("Q (5 first query)=\n");
         for (int i = 0; i < 5; i++) {
+            printf("\t");
             for (int j = 0; j < k; j++)
                 printf("%.2f ", xq[i * k + j]);
             printf("\n");
         }
         printf("I (5 first results)=\n");
         for (int i = 0; i < 5; i++) {
+            printf("\t");
             for (int j = 0; j < k; j++)
-                printf("%5zd ", I[i * k + j]);
+                printf("%zd ", I[i * k + j]);
             printf("\n");
         }
         printf("D (5 first results)=\n");
         for (int i = 0; i < 5; i++) {
+            printf("\t");
             for (int j = 0; j < k; j++)
                 printf("%.2f ", D[i * k + j]);
             printf("\n");
@@ -105,14 +130,16 @@ int main() {
         index.search(nq, xq, k, D, I);
 
         // print out results
-        printf("I (5 first results)=\n");
-        for (int i = 0; i < 5; i++) {
+        printf("I (100 first results)=\n");
+        for (int i = 0; i < 100; i++) {
+            printf("\t");
             for (int j = 0; j < k; j++)
-                printf("%5zd ", I[i * k + j]);
+                printf("%zd ", I[i * k + j]);
             printf("\n");
         }
         printf("D (5 first results)=\n");
         for (int i = 0; i < 5; i++) {
+            printf("\t");
             for (int j = 0; j < k; j++)
                 printf("%.2f ", D[i * k + j]);
             printf("\n");
